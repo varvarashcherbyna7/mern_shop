57ca946bf4efe1b073b217a26ab74a699a3f3f06
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2021-03-19 14:33:06 +0200

    create api for signin users

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import mongoose from "mongoose";
 // import { data } from "./data.js";
+import dotenv from 'dotenv';
 import userRouter from "./routers/userRouter.js";
 import productRouter from "./routers/productRouter.js";
 
+dotenv.config();
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/online-shop', {
     useNewUrlParser: true,
